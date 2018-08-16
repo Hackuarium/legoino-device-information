875231657c1b2fdd35f06fbbeebbf2796910c8ab
Luc Patiny <luc@patiny.com>
2018-08-16 07:42:20 +0200

    base project

---
+++ src/__tests__/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const legoinoDeviceInformation = require('..');
+
+describe('legoino-device-information test', () => {
+    it('Something to test', () => {
+        expect(legoinoDeviceInformation()).toBe(true);
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const defaultOptions = {};
+
+function legoinoDeviceInformation(options) {
+    options = Object.assign({}, defaultOptions, options);
+}
+
+module.exports = legoinoDeviceInformation;
